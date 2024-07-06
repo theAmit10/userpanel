8d0d35f240ca2e5f35a7448d01452476ec821a2b
theAmit10 <prasadak66@gmail.com>
2024-07-06 23:27:31 +0530

    Modified Home Screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,6 +87,176 @@ const playHistoryData = [
   },
 ];
 
+const filterdata = [
+  {
+    val: "All",
+  },
+  {
+    val: "2X",
+  },
+  {
+    val: "5X",
+  },
+  {
+    val: "10X",
+  },
+  {
+    val: "50X",
+  },
+  {
+    val: "100X",
+  },
+  {
+    val: "200X",
+  },
+];
+
+const locationdata = [
+  {
+    id: "1",
+    name: "Canada",
+    limit: "200 - 200X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "12", time: "10:00 AM" },
+      { id: "13", time: "11:00 AM" },
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "2",
+    name: "Japan",
+    limit: "200 - 200X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "12", time: "10:00 AM" },
+      { id: "13", time: "11:00 AM" },
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "3",
+    name: "Punjab",
+    limit: "200 - 200X",
+    times: [
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "4",
+    name: "Pune",
+    limit: "200 - 200X",
+    times: [
+      { id: "13", time: "11:00 AM" },
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "5",
+    name: "China",
+    limit: "100 - 100X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "6",
+    name: "India",
+    limit: "200 - 200X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "12", time: "10:00 AM" },
+      { id: "13", time: "11:00 AM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+  {
+    id: "7",
+    name: "USA",
+    limit: "200 - 200X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "12", time: "10:00 AM" },
+      { id: "13", time: "11:00 AM" },
+      { id: "14", time: "12:00 PM" },
+    ],
+  },
+  {
+    id: "8",
+    name: "Korea",
+    limit: "200 - 200X",
+    times: [
+      { id: "11", time: "09:00 AM" },
+      { id: "12", time: "10:00 AM" },
+      { id: "13", time: "11:00 AM" },
+      { id: "14", time: "12:00 PM" },
+      { id: "15", time: "01:00 PM" },
+      { id: "16", time: "02:00 PM" },
+      { id: "17", time: "03:00 PM" },
+    ],
+  },
+];
+
+const timedata = [
+  {
+    val : "09:00 AM"
+  },
+  {
+    val : "10:00 AM"
+  },
+  {
+    val : "11:00 AM"
+  },
+  {
+    val : "12:00 PM"
+  },
+  {
+    val : "01:00 PM"
+  },
+
+  {
+    val : "02:00 PM"
+  },
+  {
+    val : "03:00 PM"
+  },
+
+  {
+    val : "04:00 PM"
+  },
+  {
+    val : "04:00 PM"
+  },
+  {
+    val : "06:00 PM"
+  },
+
+  {
+    val : "07:00 PM"
+  },
+  {
+    val : "08:00 PM"
+  },
+ 
+]
+
 const Home = () => {
   return (
     <div className="main-parent">
@@ -649,7 +819,16 @@ const Home = () => {
           {/** Result list contatiner */}
           <div className="resultlistcontainer">
             {playHistoryData.map((item, index) => (
-              <div className="resultlistcontentcontainer">
+              <div
+                className="resultlistcontentcontainer"
+                style={{
+                  background:
+                    index % 2 === 0
+                      ? "linear-gradient(180deg, #7EC630, #3D6017)"
+                      : "linear-gradient(180deg, #1993FF, #0F5899)",
+                }}
+                key={index}
+              >
                 <div
                   style={{
                     height: "25%",
@@ -715,7 +894,115 @@ const Home = () => {
           </div>
 
           {/** filter contatiner */}
+
+          <div className="filtercontainer">
+            {filterdata.map((item, index) => (
+              <div className="filtercontent">
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "18px",
+                  }}
+                >
+                  {item.val}
+                </label>
+              </div>
+            ))}
+          </div>
+
           {/** Location contatiner */}
+          <div className="locationcontainer">
+            <div className="leftlocation">
+              <div className="leftlocationcontent">
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "18px",
+                  }}
+                >
+                  Pune
+                </label>
+
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "10px",
+                  }}
+                >
+                  200-200x
+                </label>
+              </div>
+
+              <div className="leftlocationcontent">
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "18px",
+                  }}
+                >
+                  Pune
+                </label>
+
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "10px",
+                  }}
+                >
+                  200-200x
+                </label>
+              </div>
+
+              <div className="leftlocationcontent">
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "18px",
+                  }}
+                >
+                  Pune
+                </label>
+
+                <label
+                  style={{
+                    color: "white",
+                    fontFamily: FONT.HELVETICA_REGULAR,
+                    fontSize: "10px",
+                  }}
+                >
+                  200-200x
+                </label>
+              </div>
+            </div>
+
+            <div className="rightlocation">
+              
+                {
+                  timedata.map((item,index) => (
+                    <div className="rightlocationcontent">
+
+                    <label
+                    style={{
+                      color: "white",
+                      fontFamily: FONT.HELVETICA_REGULAR,
+                      fontSize: "60%",
+                    }}
+                  >
+                    {item.val}
+                  </label>
+                  </div>
+                  ))
+                }
+                
+             
+            </div>
+          </div>
         </div>
 
         <div className="rightcontainer">
