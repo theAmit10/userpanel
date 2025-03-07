cec57e774360f736921f56de5d9a54e621d44307
theAmit10 <prasadak66@gmail.com>
2025-03-07 15:55:51 +0530

    skrill completed

--- src/components/deposit/PartnerSkrill.jsx
+++ src/components/deposit/PartnerSkrill.jsx
@@ -8,7 +8,11 @@ import { flushSync } from "react-dom";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useCreateDepositMutation } from "../../redux/api";
+import {
+  useCreateDepositMutation,
+  useCreateSkrillAccountMutation,
+  useDeleteSkrillAccountMutation,
+} from "../../redux/api";
 import { showErrorToast, showSuccessToast } from "../helper/showErrorToast";
 import axios from "axios";
 import UrlHelper from "../../helper/UrlHelper";
@@ -19,6 +23,8 @@ import { FaCopy } from "react-icons/fa";
 import { NodataFound } from "../helper/NodataFound";
 import { serverName } from "../../redux/store";
 import { PiSubtitles } from "react-icons/pi";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import Loader from "../molecule/Loader";
 
 const upiapidata = [
   { name: "Wasu", upiid: "9876543210@ybl", id: "1" },
@@ -56,8 +62,6 @@ function PartnerSkrill({ selectingPaymentType }) {
     setSelecetedItem("");
   };
 
-  const [createDeposit, { isLoading, error }] = useCreateDepositMutation();
-
   const [imageSource, setImageSource] = useState(null);
 
   // For Opening PhoneStorage
@@ -67,78 +71,6 @@ function PartnerSkrill({ selectingPaymentType }) {
       setImageSource(e.target.files[0]);
     } catch (err) {
       console.log(err);
-    }
-  };
-
-  const submitDepositRequest = async () => {
-    if (!amountval) {
-      showErrorToast("Enter Deposit Amount");
-      return;
-    }
-    if (isNaN(amountval)) {
-      showErrorToast("Enter Valid Amount");
-      return;
-    }
-
-    if (!transactionval) {
-      showErrorToast("Enter Transaction Number");
-      return;
-    }
-    if (!imageSource) {
-      showErrorToast("Add Transaction Screenshot");
-      return;
-    }
-    try {
-      console.log("ELSE RUNNING");
-      console.log(amountval, transactionval, remarkval);
-
-      const formData = new FormData();
-      formData.append("amount", amountval);
-      formData.append("transactionid", transactionval);
-      formData.append("remark", remarkval);
-      formData.append("paymenttype", "Skrill");
-      formData.append("paymenttypeid", selectedItem.paymentId);
-      formData.append("username", user.name);
-      formData.append("userid", user.userId);
-      formData.append("paymentstatus", "Pending");
-
-      formData.append("receipt", imageSource);
-
-      formData.append("transactionType", "Deposit");
-
-      console.log("FORM DATA :: " + JSON.stringify(formData));
-      console.log(formData);
-
-      // Logging form data for inspection
-      for (let pair of formData.entries()) {
-        console.log(pair[0] + ": " + pair[1]);
-      }
-
-      console.log(formData); // To see the complete FormData object
-
-      const res = await createDeposit({
-        accessToken: accesstoken,
-        body: formData,
-      }).unwrap();
-
-      console.log("Success");
-      console.log(res);
-      console.log(res.message);
-
-      await showSuccessToast(res.message);
-
-      hideAllform();
-      goToPreviousPage();
-    } catch (error) {
-      console.log("Error during deposit:", error);
-      if (error.response) {
-        Toast.show({ type: "error", text1: error.response.data });
-        showErrorToast(error.response.data);
-      } else if (error.request) {
-        showErrorToast("Request was made, but no response was received");
-      } else {
-        showErrorToast(error.message);
-      }
     }
   };
 
@@ -191,6 +123,78 @@ function PartnerSkrill({ selectingPaymentType }) {
     setShowAllUpi(true);
   };
 
+  const [seletedItem, setSelectedItem] = useState("");
+  const [address, setaddress] = useState("");
+  const [paymentnote, setpaymentnote] = useState("");
+  const [
+    deleteSkrillAccount,
+    { isLoading: deleteIsLoading, isError: deleteIsError },
+  ] = useDeleteSkrillAccountMutation();
+  // FOR DELETING DATA
+
+  const deletingData = async (item) => {
+    console.log("Deleting Data");
+    setSelectedItem(item._id);
+
+    const res = await deleteSkrillAccount({
+      accesstoken: accesstoken,
+      id: item._id,
+    }).unwrap();
+
+    allTheDepositData();
+    showSuccessToast(res.message);
+  };
+
+  // FOR CREATING SKRILL ACCOUNT
+
+  const [createSkrillAccount, { isLoading, error }] =
+    useCreateSkrillAccountMutation();
+
+  const submitCreateRequest = async () => {
+    if (!address) {
+      showErrorToast("Enter address");
+      return;
+    }
+    if (!paymentnote) {
+      showErrorToast("Add payment note");
+      return;
+    } else {
+      try {
+        const body = {
+          address,
+          paymentnote,
+          userId: user.userId,
+        };
+
+        console.log("JSON BODY :: ", JSON.stringify(body));
+
+        const res = await createSkrillAccount({
+          accesstoken: accesstoken,
+          body: body,
+        }).unwrap();
+
+        showSuccessToast(res.message);
+        allTheDepositData();
+        backHandlerShowCreateUpi();
+        setaddress("");
+        setpaymentnote("");
+      } catch (error) {
+        console.log("Error during deposit:", error);
+        showErrorToast("Something went wrong");
+        // if (error.response) {
+        //   Toast.show({type: 'error', text1: error.response.data});
+        // } else if (error.request) {
+        //   Toast.show({
+        //     type: 'error',
+        //     text1: 'Request was made, but no response was received',
+        //   });
+        // } else {
+        //   Toast.show({type: 'error', text1: error.message});
+        // }
+      }
+    }
+  };
+
   return (
     <div className="udC">
       {showAllUpi && (
@@ -208,6 +212,15 @@ function PartnerSkrill({ selectingPaymentType }) {
                 Skrill Payment
               </label>
             </div>
+            <div
+              className="searchIconContainer"
+              onClick={selecetingItemForDeposit}
+              style={{
+                cursor: "pointer",
+              }}
+            >
+              <IoIosAddCircleOutline color={COLORS.white_s} size={"2.5rem"} />
+            </div>
           </div>
 
           {loadingAllData ? (
@@ -222,11 +235,7 @@ function PartnerSkrill({ selectingPaymentType }) {
                 <>
                   <div className="upipdMainContainer">
                     {allDepositdata.map((item, index) => (
-                      <div
-                        onClick={() => selecetingItemForDeposit(item)}
-                        key={item._id}
-                        className="upipdContentContainer"
-                      >
+                      <div key={item._id} className="upipdContentContainer">
                         {/** TOP */}
                         <div className="uCCTopC">
                           <div className="hdContenContainerIcon">
@@ -238,7 +247,7 @@ function PartnerSkrill({ selectingPaymentType }) {
                             />
                           </div>
 
-                          <label className="pdB">Skrill</label>
+                          {/* <label className="pdB">Skrill</label> */}
                           <label
                             className="pdB"
                             style={{
@@ -252,6 +261,37 @@ function PartnerSkrill({ selectingPaymentType }) {
                           >
                             {item.paymentStatus}
                           </label>
+                          {deleteIsLoading ? (
+                            seletedItem === item._id ? (
+                              <div
+                                style={{
+                                  width: "3rem",
+                                }}
+                              >
+                                <Loader />
+                              </div>
+                            ) : (
+                              <div
+                                onClick={() => deletingData(item)}
+                                className="hdContenContainerIcon"
+                              >
+                                <MdDelete
+                                  color={COLORS.background}
+                                  size={"2.5rem"}
+                                />
+                              </div>
+                            )
+                          ) : (
+                            <div
+                              className="hdContenContainerIcon"
+                              onClick={() => deletingData(item)}
+                            >
+                              <MdDelete
+                                color={COLORS.background}
+                                size={"2.5rem"}
+                              />
+                            </div>
+                          )}
                         </div>
                         {/** TOP */}
 
@@ -310,15 +350,15 @@ function PartnerSkrill({ selectingPaymentType }) {
             </div>
             <div className="alCreatLocationTopContaineCL">
               <label className="alCreatLocationTopContainerlabel">
-                Create Skrill Deposit
+                Create Skrill Payment
               </label>
             </div>
           </div>
           {/** TOP NAVIGATION CONTATINER */}
 
           <div className="allLocationMainContainer">
-            {/** Amount */}
-            <label className="alCLLabel">Send Amount</label>
+            {/** UPI HOLDER NAME */}
+            <label className="alCLLabel">Address</label>
             <div className="alSearchContainer">
               <div className="searchIconContainer">
                 <PiSubtitles color={COLORS.background} size={"2.5rem"} />
@@ -326,16 +366,14 @@ function PartnerSkrill({ selectingPaymentType }) {
 
               <input
                 className="al-search-input"
-                type="number"
-                name="amount"
-                placeholder="Enter amount"
-                value={amountval}
-                onChange={(e) => setAmountval(e.target.value)}
+                placeholder="Enter address"
+                value={address}
+                onChange={(e) => setaddress(e.target.value)}
               />
             </div>
 
-            {/** Transaction number */}
-            <label className="alCLLabel">Transaction number</label>
+            {/** PAYMENT NOTE */}
+            <label className="alCLLabel">Note</label>
             <div className="alSearchContainer">
               <div className="searchIconContainer">
                 <PiSubtitles color={COLORS.background} size={"2.5rem"} />
@@ -343,50 +381,9 @@ function PartnerSkrill({ selectingPaymentType }) {
 
               <input
                 className="al-search-input"
-                type="text"
-                name="transaction"
-                placeholder="Enter transaction number"
-                value={transactionval}
-                onChange={(e) => setTransactionval(e.target.value)}
-              />
-            </div>
-            {/** RECEIPT */}
-
-            {/** TITLE */}
-            <label className="alCLLabel">Upload Receipt</label>
-            <div className="alSearchContainer">
-              <div className="searchIconContainer">
-                <PiSubtitles color={COLORS.background} size={"2.5rem"} />
-              </div>
-
-              <div className="imageContainerAC">
-                <input
-                  className="al-search-input"
-                  placeholder="Receipt"
-                  type="file"
-                  name="file"
-                  onChange={selectDoc}
-                  accept="image/*"
-                />
-              </div>
-            </div>
-
-            <label className="alCLLabel">Remark</label>
-            <div className="alSearchContainer">
-              <div className="searchIconContainer">
-                <PiSubtitles color={COLORS.background} size={"2.5rem"} />
-              </div>
-
-              <input
-                className="al-search-input"
-                style={{
-                  minHeight: "5rem",
-                }}
-                type="text"
-                name="remark"
-                placeholder="Enter remark"
-                value={remarkval}
-                onChange={(e) => setRemarkval(e.target.value)}
+                placeholder="Enter note"
+                value={paymentnote}
+                onChange={(e) => setpaymentnote(e.target.value)}
               />
             </div>
           </div>
@@ -394,7 +391,7 @@ function PartnerSkrill({ selectingPaymentType }) {
           {isLoading ? (
             <LoadingComponent />
           ) : (
-            <div className="alBottomContainer" onClick={submitDepositRequest}>
+            <div className="alBottomContainer" onClick={submitCreateRequest}>
               <label className="alBottomContainerlabel">Submit</label>
             </div>
           )}
